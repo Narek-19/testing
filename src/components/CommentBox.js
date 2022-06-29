b6b0e300f71db8f51fe3c9a50cd87df7c09813d1
ArtyomPC <artyom@skipti.com>
2022-06-29 12:13:54 +0400

    added comment box for testing

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
 
-export const CommentBox =()=> {
-    return(
-        <div>
-            Comment Box
-        </div>
-    )
+export class CommentBox extends Component{
+    state = {
+        comment:"",
+        
+    };
+    handleChange =(e)=>{
+        this.setState({comment:e.target.value});
+    }
+
+    handleSubmit =(e)=>{
+        e.preventDefault();
+        
+    }
+    render(){
+        return <form onSubmit={this.handleSubmit}>
+            <h4>Add a Comment</h4>
+            <textarea onChange = {this.handleChange} value = {this.state.comment}/>
+            <div>
+                <button>Submit Comment</button>
+            </div>
+        </form>;
+        
+    }
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,31 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import {App} from '../App';
+import {shallow} from 'enzyme';
 
-it('shows a comment box', ()=>{
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+import {App} from '../App';
+import {CommentBox} from "../CommentBox";
+import {CommentList} from "../CommentList";
+let wrapped;
+
+beforeEach( ()=> {
+    wrapped = shallow(<App/>);
+})
+
+it('shows a comment box', () => {
+    // MANUAL
+
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App/>, div);
 
     //Loks inside the div
 
-    expect(div.innerHTML).toContain('Comment Box');
-    ReactDOM.unmountComponentAtNode(div);
+    // expect(div.innerHTML).toContain('Comment Box');
+    // ReactDOM.unmountComponentAtNode(div);
+
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it("shows a comment list", () => {
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentList).length).toEqual(1);
+})
